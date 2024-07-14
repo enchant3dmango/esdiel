17821c4bbfe3d9a7da33a63e51678a5b9f65a07f
Andre Sitorus <andre.yehuda@sirclo.com>
2024-07-15 02:26:51 +0700

    fix: update shared_credentials_files inappropriate value

--- serverless-data-lake/providers.tf
+++ serverless-data-lake/providers.tf
@@ -1,5 +1,5 @@
 # Configure the AWS Provider
 provider "aws" {
-  shared_credentials_files = "$HOME/.aws/credentials"
+  shared_credentials_files = ["$HOME/.aws/credentials"]
   region                   = var.aws_region
 }
