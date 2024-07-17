5ca4c019513044e883faeba0ff576ac92dd8a036
Andre Sitorus <andre.yehuda@sirclo.com>
2024-07-17 17:45:01 +0700

    fix: adjust outputs to main file changes

--- serverless-data-lake/terraform/outputs.tf
+++ serverless-data-lake/terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "s3_bucket_name" {
   description = "The name of the S3 bucket"
-  value       = module.s3_bucket.s3_bucket_id
+  value       = module.s3.s3_bucket_id
 }
 
 output "lambda_function_name" {
   description = "The name of the Lambda function"
-  value       = module.lambda_function.lambda_function_name
+  value       = module.lambda.lambda_function_name
 }
 
 output "glue_etl_job_name" {
