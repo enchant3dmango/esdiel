9403090fef727f862684cc51e8ad05bcaa52f053
Andre Sitorus <andre.yehuda@sirclo.com>
2024-07-17 23:35:54 +0700

    style: update lambda function handler name

--- serverless-data-lake/lambda_function.py
+++ serverless-data-lake/lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 import os
 
 
-def lambda_handler(event, context):
+def handler(event, context):
     glue = boto3.client("glue")
     response = glue.start_job_run(JobName=os.environ["GLUE_JOB_NAME"])
 
