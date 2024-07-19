3c4fcea32e4c04205748196d398f9385c52d16b6
Andre Sitorus <andre.yehuda@sirclo.com>
2024-07-19 22:49:35 +0700

    style: remove unused import

--- serverless-data-lake/scripts/glue_etl_script.py
+++ serverless-data-lake/scripts/glue_etl_script.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
